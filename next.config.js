eccfeb61823d827eab1396ea9d4fd9d9d8ecbc3b
BarryZed <64117296+Barrryy@users.noreply.github.com>
2026-02-06 03:21:58 +0800

    关闭多语言支持

--- next.config.js
+++ next.config.js
@@ -113,6 +113,9 @@ const nextConfig = {
         defaultLocale: BLOG.LANG,
         // 支持的所有多语言,按需填写即可
         locales: locales
+
+        localeDetection: false
+        
       },
   images: {
     // 图片压缩和格式优化
