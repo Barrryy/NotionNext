d9b6b0b291e6651b830fae742315b0329d82f357
BarryZed <64117296+Barrryy@users.noreply.github.com>
2026-02-06 02:07:43 +0800

    rss fix

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [16, 7], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'BarryZed', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '专注于眼前的事物吧。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.barryzed.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.barryzed.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '游戏,编程,个人', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '黑ICP备2025035555', // 备案号 闽ICP备XXXXXX
